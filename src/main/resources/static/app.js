fa7a335ec646d88d5925fae2aa31c8764811f77a
Dibyajyoti136 <dibyajyoti9282@gmail.com>
2026-06-11 23:52:50 +0530

    connect frontend

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,6 +3,7 @@ const chatPage = document.getElementById("chatPage");
 
 const joinBtn = document.getElementById("joinBtn");
 const usernameInput = document.getElementById("username");
+const contactList=document.getElementById("contactList");
 
 const messageInput = document.getElementById("messageInput");
 const sendBtn = document.getElementById("sendBtn");
@@ -12,7 +13,7 @@ const contacts = document.querySelectorAll(".contact");
 const chatUser = document.getElementById("chatUser");
 
 /* LOGIN */
-const StompClient=null;
+let StompClient=null;
 joinBtn.addEventListener("click", () => {
 
     const username = usernameInput.value.trim();
@@ -38,7 +39,7 @@ function onConnected(){
 	StompClient.subscribe(`/user/${username}/queue/message`,onMessageReceive);
 	StompClient.subscribe('/user/public',onMessageReceive)
 	
-	StompClient.send('app/user.addUser',
+	StompClient.send('/app/user.addUser',
 		{},
 		JSON.stringify({nickName:username,fullName:username,status:"ONLINE"})
 	);
@@ -47,80 +48,129 @@ function onConnected(){
 	
 }
 
-
-async function onMessageReceive(){
+async function findAndDisplayConnectedusers(){
 	const username = usernameInput.value.trim();
-	const connectedUserResponse =await fetch('/users');
-	let connectedUsers= await connectedUserResponse.json();
-	connectedUsers=connectedUsers.filter(user=>user.nickName !== username);
+		const connectedUserResponse =await fetch('/users');
+		let connectedUsers= await connectedUserResponse.json();
+		connectedUsers=connectedUsers.filter(user=>user.nickName !== username);
+		
+		connectedUsers.forEach(user=>{
+		let li = document.createElement("li");
+		li.className = "contact";
+		li.dataset.user = user.nickName;
+
+		
+		li.innerHTML = `
+		    <span class="dot online"></span>
+		    ${user}
+		`;
+
+		contactList.appendChild(li);})
+		
+		/* CONTACT CHANGE */
+
+		contactList.forEach(contact => {
+
+		    contact.addEventListener("click", () => {
+
+		        contactList.forEach(c =>
+		            c.classList.remove("active")
+		        );
+
+		        contact.classList.add("active");
+
+		        const user =
+		            contact.dataset.user;
+
+		        chatUser.textContent = user;
+
+		        
+			fetchAndDisplayUserchat().then();
+		    });
+			
+			async function fetchAndDisplayUserchat(){
+				const userChatResponse=await fetch('/messages/${nickname}/${contact}');
+				const userChat=await userChatResponse.json();
+				
+				messages.innerHTML=` 
+				`
+				messages.innerHTML = `
+						            <div class="message received">
+						                Start chatting with ${user}
+						            </div>`;
+									
+				userChat.forEach(chat=>{
+					loadMessage(chat.senderId,chat.recepientId);
+				})					
+			}
+
+		});
+		
+		
 	
-	connectedUsers.forEach(user=>{
-	let li = document.createElement("li");
-	li.className = "contact";
-	li.dataset.user = user.Nickname;
+}
 
-	li.innerHTML = `
-	    <span class="dot online"></span>
-	    Sneha
-	`;
 
-	contactList.appendChild(li);})
-	/* CONTACT CHANGE */
+function loadMessage(a,b){
+	const messageContainer=document.createElement("div");
+	messageContainer.classList.add(
+			        "message",
+			        "sent"
+			    );
 
-	contacts.forEach(contact => {
+				msg.textContent = text;
 
-	    contact.addEventListener("click", () => {
+				messages.appendChild(msg);
 
-	        contacts.forEach(c =>
-	            c.classList.remove("active")
-	        );
+				messageInput.value = "";
 
-	        contact.classList.add("active");
+				messages.scrollTop =
+				    messages.scrollHeight;
+					
+					event.preventDefault();
+	const username = usernameInput.value.trim();
+	if(a==username){
+	messages.appendChild=(messageContainer);}
+	else{
+		messages.appendChild=(messageContainer)
+	}
+}
 
-	        const user =
-	            contact.dataset.user;
 
-	        chatUser.textContent = user;
 
-	        messages.innerHTML = `
-	            <div class="message received">
-	                Start chatting with ${user}
-	            </div>
-	        `;
-	    });
-
-	});
+async function onMessageReceive(payload){
+	await findAndDisplayConnectedusers();
+	const message=JSON.parse(payload.body);
+	loadMessage(message.senderId ,message.content)
+	
 }
 
 
 
 /* SEND MESSAGE */
 
-function sendMessage(){
+function sendMessage(event){
 
     const text =
         messageInput.value.trim();
-
+		let chatMessage=null;
     if(text === ""){
         return;
     }
+	
+	if(text && StompClient){
+		 chatMessage={
+			senderId:nickName,
+			recepientId:messageContent,
+			content:text,
+			timestamp:new Date()
+			
+		}
+		
+		StompClient.send("/app/chat",{},JSON.stringify(chatMessage))
+	}
+	loadMessage(chatMessage.senderId,chatMessage.recepientId);
 
-    const msg =
-        document.createElement("div");
-
-    msg.classList.add(
-        "message",
-        "sent"
-    );
-
-    msg.textContent = text;
-
-    messages.appendChild(msg);
-
-    messageInput.value = "";
-
-    messages.scrollTop =
-        messages.scrollHeight;
 }
 
 sendBtn.addEventListener(
